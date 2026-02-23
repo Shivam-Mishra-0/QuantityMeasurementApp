0c7983eb013b6058c509546faebddec6d5a3137f
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-02-23 13:01:25 +0530

    [Shivam] : updated FeetTest file

--- src/test/java/com/quantitymeasurement/FeetTest.java
+++ src/test/java/com/quantitymeasurement/FeetTest.java
@@ -1,7 +1,7 @@
 
 package com.quantitymeasurement;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
